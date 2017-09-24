c51dca747a2a91574b6cfbc6c6ce77b64c9aa0cb
hyc <hycgreen@gmail.com>
2017-09-24 16:25:35 +0800

    記帳簿路由改為改為SkillTree

--- MyBookkeeping/MyBookkeeping/Areas/Admin/AdminAreaRegistration.cs
+++ MyBookkeeping/MyBookkeeping/Areas/Admin/AdminAreaRegistration.cs
@@ -2,18 +2,24 @@
 
 namespace MyBookkeeping.Areas.Admin
 {
-    public class AdminAreaRegistration : AreaRegistration 
+    public class AdminAreaRegistration : AreaRegistration
     {
-        public override string AreaName 
+        public override string AreaName
         {
-            get 
+            get
             {
                 return "Admin";
             }
         }
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+        public override void RegisterArea(AreaRegistrationContext context)
         {
+            context.MapRoute(
+                             "skilltree",
+                             "skilltree/{action}/{id}",
+                             new { controller = "Journal", action = "Index", id = UrlParameter.Optional }
+                            );
+
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
